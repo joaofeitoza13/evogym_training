bea831b5e75374efa4fc4320c24fb2b929b3373d
João Feitoza <joaofeitoza.13@gmail.com>
2023-05-19 19:21:33 -0300

    feat: create home page component and some changes in main component

---
+++ src/scenes/home/Home.tsx
@@ -0,0 +1,67 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import { SelectedPage } from "@/shared/types";
+import {
+  HomePageText,
+  HomePageGraphic,
+  SponsorRedBull,
+  SponsorForbes,
+  SponsorFortune,
+} from "@/assets";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+        <div className="z-10 mt-32 md:basis-3/5">
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolveText">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes.<br />
+              World Class Studios to get the Body Shapes That you Dream of...<br />
+              Get Your Dream Body Now.<br />
+            </p>
+          </div>
+          <div className="mt-8 flex items-center gap-8 md:justify-start">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+          <img src={HomePageGraphic} alt="home-page-graphic" />
+        </div>
+      </div>
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="sponsor-redbull" />
+              <img src={SponsorForbes} alt="sponsor-forbes" />
+              <img src={SponsorFortune} alt="sponsor-fortune" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
