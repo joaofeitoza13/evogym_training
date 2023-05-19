47188aa0e8ffa202437d5279c0c60db57cc6f889
João Feitoza <joaofeitoza.13@gmail.com>
2023-05-19 19:19:09 -0300

    chore: indexed inputs for cleaner imports

---
+++ src/assets/index.ts
@@ -0,0 +1,41 @@
+import Logo from './Logo.png'
+import Arrow from './Arrow.png'
+import image1 from './image1.png'
+import image2 from './image2.png'
+import image3 from './image3.png'
+import image4 from './image4.png'
+import image5 from './image5.png'
+import image6 from './image6.png'
+import Circles from './Circles.png'
+import Sparkles from './Sparkles.png'
+import EvolveText from './EvolveText.png'
+import HomePageText from './HomePageText.png'
+import AbstractWaves from './AbstractWaves.png'
+import SponsorForbes from './SponsorForbes.png'
+import SponsorRedBull from './SponsorRedBull.png'
+import SponsorFortune from './SponsorFortune.png'
+import HomePageGraphic from './HomePageGraphic.png'
+import BenefitsPageGraphic from './BenefitsPageGraphic.png'
+import ContactUsPageGraphic from './ContactUsPageGraphic.png'
+
+export {
+  Logo,
+  Arrow,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  Circles,
+  Sparkles,
+  EvolveText,
+  HomePageText,
+  SponsorForbes,
+  AbstractWaves,
+  SponsorFortune,
+  SponsorRedBull,
+  HomePageGraphic,
+  BenefitsPageGraphic,
+  ContactUsPageGraphic,
+}
