e195cc60344b3d39b7f291bcd986f8611eeb0051
João Feitoza <joaofeitoza.13@gmail.com>
2023-05-19 19:27:35 -0300

    feat: add frame motion effects to create transitions

--- src/scenes/home/Home.tsx
+++ src/scenes/home/Home.tsx
@@ -9,7 +9,7 @@ import {
   SponsorFortune,
 } from "@/assets";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -20,21 +20,45 @@ export const Home = ({ setSelectedPage }: Props) => {
 
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+      <motion.div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+      onViewportEnter={()=>setSelectedPage(SelectedPage.Home)}>
         <div className="z-10 mt-32 md:basis-3/5">
-          <div className="md:-mt-20">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: .5 }}
+            transition={{ duration: .5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] md:before:content-evolveText">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
             <p className="mt-8 text-sm">
-              Unrivaled Gym. Unparalleled Training Fitness Classes.<br />
-              World Class Studios to get the Body Shapes That you Dream of...<br />
-              Get Your Dream Body Now.<br />
+              Unrivaled Gym. Unparalleled Training Fitness Classes.
+              <br />
+              World Class Studios to get the Body Shapes That you Dream of...
+              <br />
+              Get Your Dream Body Now.
+              <br />
             </p>
-          </div>
-          <div className="mt-8 flex items-center gap-8 md:justify-start">
+          </motion.div>
+          <motion.div
+            className="mt-8 flex items-center gap-8 md:justify-start"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: .5 }}
+            transition={{ daley: .2, duration: .5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -45,12 +69,12 @@ export const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
         <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-page-graphic" />
         </div>
-      </div>
+      </motion.div>
       {isAboveMediumScreens && (
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
